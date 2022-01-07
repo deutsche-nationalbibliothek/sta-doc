f68535ab5a11e14c21cf1963f55a6cd0dd7f7795
Thomas Bauer <tbauer2811@googlemail.com>
2022-01-07 09:13:24 +0100

    new References component for FieldDetail page.

--- components/fields/FieldDetail.js
+++ components/fields/FieldDetail.js
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
 import CodingTable from '../tables/CodingTable.js';
+import References from './References.js';
 import classes from './FieldDetail.module.css';
 
 function FieldDetail(props) {
@@ -12,15 +13,15 @@ function FieldDetail(props) {
                 coding_columns[index] = occurrence.qualifiers.type.occurrences[0].label
                 coding_rows[index] = occurrence.value
         })
-
+        const subfields = field.statements.subfields.occurrences
+        console.log('subfields',subfields)
         return (
                 <>
                 <title>{field.label}</title>
                 <section className={classes.detail}>
                 <h1>{field.label}</h1>
-                <p>{field.description}</p>
                 <h3>{field.statements.coding.label}</h3>
-                        <CodingTable rows={coding_rows} columns={coding_columns}/>
+                <CodingTable rows={coding_rows} columns={coding_columns}/>
                 <h3>{field.statements.definition.label}</h3>
                 {field.statements.definition.occurrences.map((occurrence,index) => {
                         return(
@@ -41,9 +42,22 @@ function FieldDetail(props) {
                 })}
                 <h3>{field.statements.subfields.label}</h3>
                 {field.statements.subfields.occurrences.map((subfield,index) => {
-                        console.log(subfield)
+                        // console.log(subfield)
+                        let ListDescription
+                        const descriptions = subfield.qualifiers.description
+                        descriptions ? (ListDescription = descriptions.occurrences)
+                        : ListDescription = []
+                        let ListReference
+                        const references = subfield.references
+                        references ? (ListReference = references)
+                        : ListReference = []
                         return(
-                                <h4 key={index}>{subfield.label}</h4>
+                                <Fragment key={index}>
+                                <h4>{subfield.label}</h4>
+                                {references &&
+                                        <References references={subfield.references}/>
+                                }
+                                </Fragment>
                         )
                 })}
                 </section>
---
+++ components/fields/References.js
@@ -0,0 +1,32 @@
+import { Fragment } from 'react';
+import classes from './References.module.css';
+
+function ReferenceBox(props) {
+  const referenceMap = {
+    url: (ref) => <a href={ref.value}>{ref.value}</a>,
+    default: (ref) => <p>{ref.value}</p>
+  }
+  return(
+    <div className={classes.div}>
+    {
+      Object.keys(props).map((key,index) => 
+        <Fragment key={index}>
+        {referenceMap[key] ? referenceMap[key](props[key]) : referenceMap.default(props[key])}
+        </Fragment>
+      )
+    }
+    </div>
+  )
+}
+
+function References({references}) {
+  if (references) {
+    return(
+      <>
+      <h5 className={classes.h5}>Referenzen</h5>
+      {references.map((ref,index) => <ReferenceBox key={index} {...ref} />)}
+      </>
+    )
+  }
+}
+export default References
