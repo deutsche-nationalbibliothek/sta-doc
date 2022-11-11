13c83c164cec836cf4a900ca7954482975967005
Armin Fröhlich <mail@arminfroehlich.de>
2022-11-11 09:44:21 +0100

    render Sidebar only if there are headlines

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,10 +4,12 @@ import { ContentNavigation } from './content-navigation';
 
 export const Sidebar: React.FC = () => {
   const { headlines } = useHeadlines();
+
   return (
-    <Layout.Sider theme={'light'} width={400}>
-      {headlines.length && <ContentNavigation headlines={headlines} />}
-    </Layout.Sider>
+    headlines.length > 1 && (
+      <Layout.Sider theme={'light'} width={400}>
+        <ContentNavigation headlines={headlines} />
+      </Layout.Sider>
+    )
   );
 };
-
