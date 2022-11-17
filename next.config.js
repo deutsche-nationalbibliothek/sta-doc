c5e46e1d981eba64a6518a48b76a7b56b1820c3c
Armin Fröhlich <mail@arminfroehlich.de>
2022-11-17 14:26:29 +0100

    add redirects for entries to entities

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 /**
  * @type {import('next').NextConfig}
  **/
-const withLess = require("next-with-less");
+const withLess = require('next-with-less');
 
 const nextConfig = {
   images: {
     domains: ['www.cilip.org.uk'],
   },
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/entries/:entityId',
+        destination: '/entities/:entityId',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withLess(nextConfig);
