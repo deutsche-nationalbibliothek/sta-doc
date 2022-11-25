0779a4dacc5d8002327396a42174d179b9eec306
Armin Fröhlich <mail@arminfroehlich.de>
2022-11-25 07:44:23 +0100

    add antd ConfigProvider to setup locale

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,21 +5,25 @@ import Layout from '@/components/layout';
 import IsLoadingContextProvider from '@/hooks/use-loading-state';
 import { CodingsPreferencesProvider } from '@/hooks/use-codings-preference';
 import { DataSourceProvider } from '@/hooks/use-pagetype';
+import { ConfigProvider } from 'antd';
+import deDE from 'antd/lib/locale/de_DE';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <DataSourceProvider>
-        <HeadlinesProvider>
-          <IsLoadingContextProvider>
-            <CodingsPreferencesProvider>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </CodingsPreferencesProvider>
-          </IsLoadingContextProvider>
-        </HeadlinesProvider>
-      </DataSourceProvider>
+      <ConfigProvider locale={deDE}>
+        <DataSourceProvider>
+          <HeadlinesProvider>
+            <IsLoadingContextProvider>
+              <CodingsPreferencesProvider>
+                <Layout>
+                  <Component {...pageProps} />
+                </Layout>
+              </CodingsPreferencesProvider>
+            </IsLoadingContextProvider>
+          </HeadlinesProvider>
+        </DataSourceProvider>
+      </ConfigProvider>
     </>
   );
 }
