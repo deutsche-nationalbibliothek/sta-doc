6418f3ffb9659a19d004b53ae0402b84d5c03de8
tbauer2811 <tbauer2811@googlemail.com>
2023-01-27 14:07:05 +0100

    Update sparql at RDAPROPERTIES.

--- bin/data/utils/sparql.ts
+++ bin/data/utils/sparql.ts
@@ -28,13 +28,14 @@ export const RDAPROPERTIES = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?element ?eId ?elementLabel ?assignmentId ?assignmentLabel  WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+  SELECT ?element ?eId ?elementLabel ?entitytypeId ?entitytypeLabel ?wemilevelLabel WHERE {
     { ?element prop:P2 item:Q264 . } #Element von: RDA-Eigenschaft
-    { ?element p:P124 ?assignmentProp .
-    ?assignmentProp statement:P124 ?assignment . }
+    OPTIONAL { ?element p:P124 ?assignmentProp .
+    ?assignmentProp statement:P124 ?entitytype . }
+    OPTIONAL { ?element prop:P639 ?wemilevel . }
     SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
     BIND(STRAFTER(STR(?element), '/entity/') as ?eId)
-    BIND(STRAFTER(STR(?assignment), '/entity/') as ?assignmentId)
+    BIND(STRAFTER(STR(?entitytype), '/entity/') as ?entitytypeId)
   }
   ORDER BY ASC(?elementLabel)
 `;
@@ -54,7 +55,7 @@ export const RDARULES = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?element ?eId ?elementLabel  WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+  SELECT ?element ?eId ?elementLabel  WHERE {
     { ?element prop:P2 item:Q3095 . } #Element von: RDA-Regeltext
     #SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
     SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
@@ -78,7 +79,7 @@ export const FIELDS = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?element ?eId ?elementLabel  WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+  SELECT ?element ?eId ?elementLabel  WHERE {
     { ?element prop:P2 item:Q2 . } #Element von: GND-Field
     #SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
     SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
@@ -102,7 +103,7 @@ export const SUBFIELDS = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?element ?eId ?elementLabel  WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+  SELECT ?element ?eId ?elementLabel  WHERE {
     { ?element prop:P2 item:Q3 . } #Element von: GND-Subfield
     #SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
     SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
@@ -120,7 +121,7 @@ export const CODINGS = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?eId ?elementLabel ?codingTypeLabel ?coding  WHERE { # ?element
+  SELECT ?eId ?elementLabel ?codingTypeLabel ?coding  WHERE {
     { ?element p:P4 ?codingProp . }
     OPTIONAL{ ?codingProp statement:P4 ?coding . }
     OPTIONAL{ ?codingProp qualifier:P3 ?codingType }
@@ -139,7 +140,7 @@ export const CODINGSEXTRA = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?eId ?elementLabel ?elementOfId ?elementOfLabel ?formatId ?formatLabel ?coding  WHERE { # ?element
+  SELECT ?eId ?elementLabel ?elementOfId ?elementOfLabel ?formatId ?formatLabel ?coding  WHERE {
     { ?element p:P4 ?codingProp . }
     OPTIONAL
     { ?element prop:P2 ?elementOf . }
@@ -163,9 +164,8 @@ export const ELEMENTS_OF = (apiUrl: API_URL) => `
   PREFIX item: <${apiUrl}/entity/>
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
-  SELECT DISTINCT ?eId ?elementOfId ?elementOfLabel
-  WHERE {
-    ?element rdfs:label ?elementLabel_de FILTER (LANG(?elementLabel_de) = "de") .
+  SELECT DISTINCT ?eId ?elementOfId ?elementOfLabel WHERE {
+    { ?element rdfs:label ?elementLabel_de FILTER (LANG(?elementLabel_de) = "de") . }
     { ?element prop:P2 ?elementOf . }
     SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
     BIND(STRAFTER(STR(?element), '/entity/') as ?eId)
@@ -190,7 +190,7 @@ PREFIX item: <${apiUrl}/entity/>
 PREFIX qualifier: <${apiUrl}/prop/qualifier/>
 PREFIX statement: <${apiUrl}/prop/statement/>
 
-SELECT ?eId ?elementLabel ?notationLabel  WHERE { # ?element ?codingTypeLabel ?coding
+SELECT ?eId ?elementLabel ?notationLabel  WHERE {
 ?element prop:P643 ?notation .
 #OPTIONAL{ ?codingProp statement:P4 ?coding . }
 #OPTIONAL{ ?codingProp qualifier:P3 ?codingType }
@@ -217,7 +217,7 @@ export const LABELEN = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?eId ?elementLabel ?assignmentId ?assignmentLabel WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+  SELECT ?eId ?elementLabel ?assignmentId ?assignmentLabel WHERE {
     { ?element prop:P110 ?assignment . } #Schema
     OPTIONAL
     { ?element p:P2 ?assignmentProp .
@@ -237,7 +237,7 @@ export const propertyItemList = (apiUrl: API_URL) => `
     PREFIX item: <${apiUrl}/entity/>
     PREFIX qualifier: <${apiUrl}/prop/qualifier/>
     PREFIX statement: <${apiUrl}/prop/statement/>
-    SELECT ?eId ?elementLabel ?assignmentId ?assignmentLabel WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+    SELECT ?eId ?elementLabel ?assignmentId ?assignmentLabel WHERE {
         ?element rdfs:label ?type .
         OPTIONAL
         { ?element p:P2 ?assignmentProp .
@@ -265,7 +265,7 @@ export const LABELDE = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?eId ?elementLabel  WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+  SELECT ?eId ?elementLabel  WHERE {
     { ?element prop:P110 ?assignment . } #Schema: datamodel documentation
     SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
     BIND(STRAFTER(STR(?element), '/entity/') as ?eId)
@@ -281,7 +281,7 @@ export const LABELDE2 = (apiUrl: API_URL) => `
     PREFIX item: <${apiUrl}/entity/>
     PREFIX qualifier: <${apiUrl}/prop/qualifier/>
     PREFIX statement: <${apiUrl}/prop/statement/>
-    SELECT ?eId ?elementLabel WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+    SELECT ?eId ?elementLabel WHERE {
         ?element rdfs:label ?type .
         SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
         BIND(STRAFTER(STR(?element), '/entity/') as ?eId)
@@ -304,7 +304,7 @@ export const EXAMPLES = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?element ?eId ?elementLabel  WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+  SELECT ?element ?eId ?elementLabel  WHERE {
     { ?element prop:P2 item:Q14 . } #Element von: DACH-Dokumentation: Beispiel
     #SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
     SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
@@ -327,7 +327,7 @@ export const DESCRIPTIONS = (apiUrl: API_URL) => `
   PREFIX qualifier: <${apiUrl}/prop/qualifier/>
   PREFIX statement: <${apiUrl}/prop/statement/>
 
-  SELECT ?element ?eId ?elementLabel  WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
+  SELECT ?element ?eId ?elementLabel  WHERE {
     { ?element prop:P7 ?statements . } #Aussage mit Beschreibung (P7)
     #SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
     SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
@@ -344,53 +344,11 @@ PREFIX prop: <${apiUrl}/prop/direct/>
 PREFIX item: <${apiUrl}/entity/>
 PREFIX qualifier: <${apiUrl}/prop/qualifier/>
 PREFIX statement: <${apiUrl}/prop/statement/>
-SELECT DISTINCT ?eId ?elementLabel_de ?staNotationLabel
-WHERE {
-  ?element rdfs:label ?elementLabel_de FILTER (LANG(?elementLabel_de) = "de") .
+SELECT DISTINCT ?eId ?elementLabel_de ?staNotationLabel WHERE {
+  { ?element rdfs:label ?elementLabel_de FILTER (LANG(?elementLabel_de) = "de") . }
   { ?element prop:P643 ?staNotation . }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
   BIND(STRAFTER(STR(?element), '/entity/') as ?eId)
 }
 ORDER BY ASC(?elementLabel_de)
 `;
-
-// export export const LABELEN = (apiUrl: API_URL) => `
-// PREFIX wd: <http://www.wikidata.org/entity/>
-// PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-// PREFIX wikibase: <http://wikiba.se/ontology#>
-// #PREFIX p: <http://www.wikidata.org/prop/>
-// PREFIX ps: <http://www.wikidata.org/prop/statement/>
-// PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
-// PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-// PREFIX bd: <http://www.bigdata.com/rdf#>
-
-// PREFIX p: <${apiUrl}/prop/>
-// PREFIX prop: <${apiUrl}/prop/direct/>
-// PREFIX item: <${apiUrl}/entity/>
-// PREFIX qualifier: <${apiUrl}/prop/qualifier/>
-// PREFIX statement: <${apiUrl}/prop/statement/>
-
-// SELECT ?element ?eId ?elementLabel  WHERE { # ?coding ?codingTypeLabel ?definition ?subfields ?subfieldsLabel
-// { ?element prop:P114 item:Q296 . } #Instance of schema
-// UNION
-// { ?element prop:P2 item:Q296 . } #Element of schema
-// UNION
-// { ?element prop:P110 item:Q1 . } #Schema: GND-Datenmodell
-// UNION
-// { ?element prop:P110 item:Q3113 . } #Schema: DACH-Dokumentation
-// UNION
-// { ?element prop:P110 item:Q15 . } #Datenmodell-Dokumentation
-// UNION
-// { ?element prop:P110 item:Q263 . } #RDA-Dokumentation
-// UNION
-// { ?element prop:P110 item:Q14 . } #GND-Beispiel
-// UNION
-// { ?element prop:P2 item:Q3 . } #Element von: GND-Unterfeld
-// UNION
-// { ?element prop:P2 item:Q1315 . } #Element von: Datenformat
-// #SERVICE wikibase:label { bd:serviceParam wikibase:language "de" }
-// SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
-// BIND(STRAFTER(STR(?element), '/entity/') as ?eId)
-// }
-// ORDER BY ASC(?elementLabel)
-// `
