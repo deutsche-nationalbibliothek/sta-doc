4e6b967a6b843fc43e44725abab650cd5e974c2e
Armin Fröhlich <mail@arminfroehlich.de>
2023-01-31 08:32:41 +0100

    fix test warning 'not wrapped in act(...)'
    Warning: An update to ForwardRef inside a test was not wrapped in act(...)
    ant-design issues 30964

--- __tests__/pages/entities/[entityId].tsx
+++ __tests__/pages/entities/[entityId].tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { genericMatchSnapshot } from '../../../test-utils';
 import { EntityDetails } from '../../../features/entity/components/details';
 import entities from '../../../data/parsed/entities.json';
+import { Entity } from '../../../types/parsed/entity';
+import { screen, waitFor } from '@testing-library/react';
 jest.mock('next/router', () => require('next-router-mock'));
 
 describe('renders EntityDetails', () => {
-  Object.keys(entities)
-    .forEach((entityId) => {
-      it(`${entityId} unchanged`, () => {
-        genericMatchSnapshot(
-          <EntityDetails entity={entities[entityId].entity} />,
-          entityId
-        );
+  Object.keys(entities).forEach((entityId) => {
+    it(`${entityId} unchanged`, async () => {
+      const { entity } = entities[entityId] as { entity: Entity };
+      genericMatchSnapshot(<EntityDetails entity={entity} />, entityId);
+      await waitFor(() => {
+        expect(screen.getByText(entity.headline?.title as string));
       });
     });
+  });
 });
