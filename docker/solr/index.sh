96cdf98a251aaccee221d6d6381ca5a0e1c8cb29
Armin Fröhlich <mail@arminfroehlich.de>
2023-02-23 08:42:28 +0100

    update solr docker instructions

--- docker/solr/index.sh
+++ docker/solr/index.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Create a new Solr collection
-solr create -c entities -s 2 -rf 2 -p 8983 -d /var/solr/data/entities
+solr create -c entities -s 2 -rf 2 -p 8983 -d /opt/solr-9.1.1/entities
 
-cd /opt/solr/data # || echo 'no data found' && exit
+cd /opt/solr-9.1.1/data # || echo 'no data found' && exit
 
 # Index each split file into the new collection
 for file in *.json; do
