21a2a4472da4f38742689bef83414c4fc821aa96
tbauer2811 <tbauer2811@googlemail.com>
2023-03-01 16:09:00 +0000

    Remove unneeded notations file in api.

--- pages/api/entities/notations.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import notations from '@/data/parsed/sta-notations.json';
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json(notations);
-};
