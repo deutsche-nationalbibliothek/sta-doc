352ac0cfe7865183cf3a1bd9dcfd2bd1f1e57dc3
Thomas Bauer <tbauer2811@googlemail.com>
2023-03-03 10:05:38 +0100

    Fix Url path in copy-headline-anchor-link.

--- components/copy-headline-anchor-link.tsx
+++ components/copy-headline-anchor-link.tsx
@@ -18,7 +18,7 @@ export const CopyHeadlineAnchorLink: React.FC<CopyIconProps> = ({
 
   const onCopy = () => {
     const hasCopied = copy(
-      `${window.location.origin}${router.asPath.match(/.*(?=#.*)|.*/)[0]
+      `${window.location.origin}/doc${router.asPath.match(/.*(?=#.*)|.*/)[0]
       }#${anchor}`
     );
     if (hasCopied) {
