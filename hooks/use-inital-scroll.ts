6ccf5909201f867bafb4709c65d5f71446b5dd2e
Thomas Bauer <tbauer2811@googlemail.com>
2023-03-17 12:20:11 +0100

    Update useInitialScroll behavior.

--- hooks/use-inital-scroll.ts
+++ hooks/use-inital-scroll.ts
@@ -1,9 +1,6 @@
-import { useRouter } from '@/lib/next-use-router';
 import { useEffect } from 'react';
 
 export const useInitialScroll = (shouldDo: boolean) => {
-  console.log('use initial scroll')
-  const router = useRouter();
   useEffect(() => {
     const { hash } = window.location;
     if (hash && shouldDo) {
