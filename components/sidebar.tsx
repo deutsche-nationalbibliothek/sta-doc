ba0692f74498d33c5f41aebc9ce8f6539665d67a
Armin Fröhlich <mail@arminfroehlich.de>
2023-04-05 09:56:06 +0200

    fix expandedKeys in TableOfContent, add custom hook

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { TableOfContent } from '@/features/entity/components/tabel-of-content';
+import { TableOfContent } from '@/features/entity/components/table-of-content';
 import { useInitialHeadlines } from '@/hooks/initial-headlines';
 import { Divider, Layout } from 'antd';
 import React from 'react';
@@ -8,40 +8,36 @@ export const Sidebar: React.FC = () => {
   const containerRef = React.useRef<HTMLDivElement>(null);
   const dividerRef = React.useRef<HTMLDivElement>(null);
 
-  return (
-    <>
-      {headlines && headlines.length > 1 && (
-        <Layout.Sider theme={'light'} width={'100%'} style={{ height: '100%' }}>
-          <div
-            ref={containerRef}
-            className="main-layout-height"
+  return headlines && headlines.length > 1 ? (
+    <Layout.Sider theme={'light'} width={'100%'} style={{ height: '100%' }}>
+      <div
+        ref={containerRef}
+        className="main-layout-height"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <div ref={dividerRef}>
+          <Divider
             style={{
-              display: 'flex',
-              flexDirection: 'column',
+              marginTop: 'var(--topbar-padding-y)',
+              marginBottom: 'var(--topbar-padding-y)',
             }}
-          >
-            <div ref={dividerRef}>
-              <Divider
-                style={{
-                  marginTop: 'var(--topbar-padding-y)',
-                  marginBottom: 'var(--topbar-padding-y)',
-                }}
-              />
-            </div>
-            <TableOfContent
-              headlines={headlines}
-              containerRef={containerRef}
-              dividerRef={dividerRef}
-            />
-            <Divider
-              style={{
-                marginTop: 'var(--topbar-padding-y)',
-                marginBottom: 'var(--topbar-padding-y)',
-              }}
-            />
-          </div>
-        </Layout.Sider>
-      )}
-    </>
-  );
+          />
+        </div>
+        <TableOfContent
+          headlines={headlines}
+          containerRef={containerRef}
+          dividerRef={dividerRef}
+        />
+        <Divider
+          style={{
+            marginTop: 'var(--topbar-padding-y)',
+            marginBottom: 'var(--topbar-padding-y)',
+          }}
+        />
+      </div>
+    </Layout.Sider>
+  ) : null;
 };
