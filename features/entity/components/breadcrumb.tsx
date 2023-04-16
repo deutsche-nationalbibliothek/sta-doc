2813e6fd21827c0a016bc881d8629353f12e227d
Armin Fröhlich <mail@arminfroehlich.de>
2023-04-16 15:14:20 +0200

    improve breadcrumb

--- features/entity/components/breadcrumb.tsx
+++ features/entity/components/breadcrumb.tsx
@@ -1,16 +1,32 @@
 import { CopyHeadlineAnchorLink } from '@/components/copy-headline-anchor-link';
+import { useEntity } from '@/hooks/entity-provider';
 import { useHeadlines } from '@/hooks/headlines';
+import { Namespace } from '@/types/namespace';
 import { Breadcrumb as AntdBreadcrumb, Tooltip, theme } from 'antd';
-import { truncate } from 'lodash';
+import { compact, truncate } from 'lodash';
 import { Fragment } from 'react';
 
 export const Breadcrumb: React.FC = () => {
   const { currentHeadlinesPath, showHeadlines } = useHeadlines();
   const { token } = theme.useToken();
+  const { entity } = useEntity();
   // colorTextSecondary
+  const entityDetails = entity
+    ? compact([entity.namespace, entity.title, entity.label]).map((x) => ({
+        title: x,
+        namespace: entity.namespace,
+      }))
+    : [];
+
+  const breadcrumbItems: {
+    key?: string;
+    namespace?: Namespace;
+    title: string;
+  }[] = [...entityDetails, ...currentHeadlinesPath];
   return (
     <div
       css={{
+        fontSize: 'small',
         background: 'var(--dark-gray)',
         '& .ant-breadcrumb a': {
           color: token.colorTextSecondary,
@@ -20,30 +36,44 @@ export const Breadcrumb: React.FC = () => {
       <AntdBreadcrumb
         css={{
           height: 'var(--breadcrumb-height)',
+          padding: '0 1% 0 1%',
         }}
         separator=""
       >
         {showHeadlines &&
-          currentHeadlinesPath.map(({ key, title, namespace }, index) => {
-            const isLastIndex = index === currentHeadlinesPath.length - 1;
+          breadcrumbItems.map(({ key, title, namespace }, index) => {
+            const isLastIndex = index === breadcrumbItems.length - 1;
             return (
-              <Fragment key={key}>
-                <AntdBreadcrumb.Item>
-                  <Tooltip
-                    placement="bottom"
-                    title={
-                      <>
-                        {title} <CopyHeadlineAnchorLink anchor={key} />
-                      </>
-                    }
-                  >
-                    <a href={`#${key}`}>
+              <Fragment key={key ?? title}>
+                <AntdBreadcrumb.Item
+                  css={{
+                    paddingTop: 2,
+                    fontSize: 'small',
+                  }}
+                >
+                  {key ? (
+                    <Tooltip
+                      placement="bottom"
+                      title={
+                        <>
+                          {title} <CopyHeadlineAnchorLink anchor={key} />
+                        </>
+                      }
+                    >
+                      <a href={`#${key}`}>
+                        {isLastIndex ? title : truncate(title, { length: 64 })}
+                      </a>
+                    </Tooltip>
+                  ) : (
+                    <a>
                       {isLastIndex ? title : truncate(title, { length: 64 })}
                     </a>
-                  </Tooltip>
+                  )}
                 </AntdBreadcrumb.Item>
                 {!isLastIndex && (
-                  <AntdBreadcrumb.Separator key={`${key}-seperator`}>
+                  <AntdBreadcrumb.Separator
+                    key={namespace ? `${namespace}-seperator` : ''}
+                  >
                     <span className={namespace ? `${namespace}-seperator` : ''}>
                       /
                     </span>
