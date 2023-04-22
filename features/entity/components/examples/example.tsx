48b065f87f8958267ece415b2aaee9b7265ad461
Armin Fröhlich <mail@arminfroehlich.de>
2023-04-22 07:28:27 +0200

    mv RdaExample to seperate file

--- features/entity/components/examples/example.tsx
+++ features/entity/components/examples/example.tsx
@@ -1,4 +1,3 @@
-import { ColumnsTypes, Table } from '@/components/table';
 import {
   CodingsPreference,
   useCodingsPreference,
@@ -9,22 +8,21 @@ import { PrefCodingsLabel } from '@/types/parsed/coding';
 import { Statement, Entity } from '@/types/parsed/entity';
 import { Property } from '@/types/property';
 import { Card, Tag, Typography, theme } from 'antd';
-import { compact, flattenDeep } from 'lodash';
+import { compact } from 'lodash';
 import React from 'react';
-import { EntityLink } from '../preview/link';
 import { Statements } from '../statements';
-import { StringGroupsStatement } from '../statements/string-groups';
+import { RdaExample } from './rda-example';
 
-interface ExampleProps {
+export interface ExampleProps {
   entity: Entity;
   codingsPreferences: CodingsPreference[];
 }
 
-interface PreData {
+export interface PreData {
   values: { key: string; value: string }[];
   statement: Statement;
 }
-interface TableData extends PreData {
+export interface TableData extends PreData {
   label: string;
 }
 
@@ -34,7 +32,7 @@ interface ExampleValues {
   'PICA+': { value: string; coding?: string }[][];
 }
 
-const nonDefaultRenderProperties = [
+export const nonDefaultRenderProperties = [
   Property.description,
   Property['description-(at-the-end)'],
 ];
@@ -260,96 +258,5 @@ const ExampleCodingCard: React.FC<ExampleCodingCardProps> = ({
         </Typography.Paragraph>
       ))}
     </Card>
-  );
-};
-
-const RdaExample: React.FC<ExampleProps> = ({ entity }) => {
-  const relevantStatemants = entity.statements.body.filter(
-    (statement) => !nonDefaultRenderProperties.includes(statement.property)
-  );
-  const preData: Record<string, PreData> = flattenDeep(
-    compact(
-      relevantStatemants.map(
-        (statement) =>
-          statement.stringGroups &&
-          statement.stringGroups.map((stringStatement) =>
-            stringStatement.values.map((stringValue) => ({
-              key: statement.label,
-              statement,
-              value: stringValue.value,
-            }))
-          )
-      )
-    )
-  ).reduce(
-    (
-      acc,
-      stringValueMeta: { key: string; value: string; statement: Statement }
-    ) => {
-      if (acc[stringValueMeta.key]) {
-        acc[stringValueMeta.key] = {
-          ...acc[stringValueMeta.key],
-          values: [...acc[stringValueMeta.key].values, stringValueMeta],
-        };
-      } else {
-        acc[stringValueMeta.key] = {
-          values: [stringValueMeta],
-          statement: stringValueMeta.statement,
-        };
-      }
-      return acc;
-    },
-    {} as Record<string, PreData>
-  );
-  const data: TableData[] = Object.entries(preData).map(([key, value]) => ({
-    label: key,
-    values: value.values,
-    statement: value.statement,
-  }));
-
-  const columns: ColumnsTypes<TableData> = [
-    {
-      dataIndex: 'label',
-      className: 'va-top',
-      key: 'label',
-      render: (label: string, { statement }) => {
-        return (
-          <EntityLink
-            namespace={statement.namespace}
-            id={statement.property}
-            label={label}
-          />
-        );
-      },
-      width: '33%',
-    },
-    {
-      dataIndex: 'values',
-      key: 'values',
-      render: (_values, { statement }) => {
-        return (
-          statement.stringGroups && (
-            <StringGroupsStatement
-              property={statement.property}
-              statements={statement.stringGroups}
-            />
-          )
-        );
-      },
-    },
-  ];
-
-  return (
-    <>
-      {data.length > 0 && (
-        <Table<TableData>
-          dataSource={data}
-          pagination={false}
-          columns={columns}
-          showHeader={false}
-          className="example-table"
-        />
-      )}
-    </>
   );
 };
---
+++ features/entity/components/examples/rda-example.tsx
@@ -0,0 +1,102 @@
+import { ColumnsTypes, Table } from '@/components/table';
+import { EntityLink } from '../preview/link';
+import { StringGroupsStatement } from '../statements/string-groups';
+import { compact, flattenDeep } from 'lodash';
+import {
+  ExampleProps,
+  PreData,
+  TableData,
+  nonDefaultRenderProperties,
+} from './example';
+import { Statement } from '@/types/parsed/entity';
+
+export const RdaExample: React.FC<ExampleProps> = ({ entity }) => {
+  const relevantStatemants = entity.statements.body.filter(
+    (statement) => !nonDefaultRenderProperties.includes(statement.property)
+  );
+  const preData: Record<string, PreData> = flattenDeep(
+    compact(
+      relevantStatemants.map(
+        (statement) =>
+          statement.stringGroups &&
+          statement.stringGroups.map((stringStatement) =>
+            stringStatement.values.map((stringValue) => ({
+              key: statement.label,
+              statement,
+              value: stringValue.value,
+            }))
+          )
+      )
+    )
+  ).reduce(
+    (
+      acc,
+      stringValueMeta: { key: string; value: string; statement: Statement }
+    ) => {
+      if (acc[stringValueMeta.key]) {
+        acc[stringValueMeta.key] = {
+          ...acc[stringValueMeta.key],
+          values: [...acc[stringValueMeta.key].values, stringValueMeta],
+        };
+      } else {
+        acc[stringValueMeta.key] = {
+          values: [stringValueMeta],
+          statement: stringValueMeta.statement,
+        };
+      }
+      return acc;
+    },
+    {} as Record<string, PreData>
+  );
+  const data: TableData[] = Object.entries(preData).map(([key, value]) => ({
+    label: key,
+    values: value.values,
+    statement: value.statement,
+  }));
+
+  const columns: ColumnsTypes<TableData> = [
+    {
+      dataIndex: 'label',
+      className: 'va-top',
+      key: 'label',
+      render: (label: string, { statement }) => {
+        return (
+          <EntityLink
+            namespace={statement.namespace}
+            id={statement.property}
+            label={label}
+          />
+        );
+      },
+      width: '33%',
+    },
+    {
+      dataIndex: 'values',
+      key: 'values',
+      render: (_values, { statement }) => {
+        return (
+          statement.stringGroups && (
+            <StringGroupsStatement
+              property={statement.property}
+              statements={statement.stringGroups}
+            />
+          )
+        );
+      },
+    },
+  ];
+
+  return (
+    <>
+      {data.length > 0 && (
+        <Table<TableData>
+          dataSource={data}
+          pagination={false}
+          columns={columns}
+          showHeader={false}
+          className="example-table"
+        />
+      )}
+    </>
+  );
+};
