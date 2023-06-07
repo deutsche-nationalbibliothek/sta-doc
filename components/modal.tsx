ad5802ec947332dd2f25849c70c775d7ea66f2ec
Thomas Bauer <tbauer2811@googlemail.com>
2023-06-07 11:33:05 +0200

    Shift draggable modal function to example component.

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,9 +1,7 @@
 import { useRouter } from '@/lib/next-use-router';
 import { css } from '@emotion/react';
 import { Modal as AntdModal, ModalProps as AntdModalProps, theme } from 'antd';
-import { useEffect, useState, useRef } from 'react';
-import type { DraggableData, DraggableEvent } from 'react-draggable';
-import Draggable from 'react-draggable';
+import { useEffect, useState } from 'react';
 
 interface ModalProps extends Omit<AntdModalProps, 'open' | 'onCancel'> {
   label?: JSX.Element | string;
@@ -14,22 +12,14 @@ interface ModalProps extends Omit<AntdModalProps, 'open' | 'onCancel'> {
 }
 
 export const Modal: React.FC<ModalProps> = (props) => {
-  const [open, setOpen] = useState(false);
-  const [disabled, setDisabled] = useState(true);
-  const [bounds, setBounds] = useState({
-    left: 0,
-    top: 0,
-    bottom: 0,
-    right: 0,
-  });
-  const draggleRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
   const { token } = theme.useToken();
   const { asPath } = useRouter();
 
   useEffect(() => {
     if (props.closeOnRouteChange) {
       return () => {
-        setOpen(false);
+        setIsOpen(false);
       };
     }
   }, [asPath, props.closeOnRouteChange]);
@@ -37,25 +27,11 @@ export const Modal: React.FC<ModalProps> = (props) => {
   const { label, ...otherProps } = props;
 
   const showModal = () => {
-    setOpen(true);
+    setIsOpen(true);
   };
 
   const onCancel = () => {
-    setOpen(false);
-  };
-
-  const onStart = (_event: DraggableEvent, uiData: DraggableData) => {
-    const { clientWidth, clientHeight } = window.document.documentElement;
-    const targetRect = draggleRef.current?.getBoundingClientRect();
-    if (!targetRect) {
-      return;
-    }
-    setBounds({
-      left: -targetRect.left + uiData.x,
-      right: clientWidth - (targetRect.right - uiData.x),
-      top: -targetRect.top + uiData.y,
-      bottom: clientHeight - (targetRect.bottom - uiData.y),
-    });
+    setIsOpen(false);
   };
 
   return (
@@ -67,41 +43,11 @@ export const Modal: React.FC<ModalProps> = (props) => {
       )}
       <AntdModal
         title={
-          // props.disableLabelOnOpen !== undefined &&
-          // (open ? '' : otherProps.title || label)
-          <div
-            style={{
-              width: '100%',
-              cursor: 'move',
-            }}
-            onMouseOver={() => {
-              if (disabled) {
-                setDisabled(false);
-              }
-            }}
-            onMouseOut={() => {
-              setDisabled(true);
-            }}
-            // fix eslintjsx-a11y/mouse-events-have-key-events
-            // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/mouse-events-have-key-events.md
-            onFocus={() => {}}
-            onBlur={() => {}}
-            // end
-          >
-            Draggable Modal
-          </div>
+          props.disableLabelOnOpen !== undefined &&
+          (isOpen ? '' : otherProps.title || label)
         }
-        open={open}
+        open={isOpen}
         onCancel={onCancel}
-        modalRender={(modal) => (
-          <Draggable
-            disabled={disabled}
-            bounds={bounds}
-            onStart={(event, uiData) => onStart(event, uiData)}
-          >
-            <div ref={draggleRef}>{modal}</div>
-          </Draggable>
-        )}
         footer={[]}
         width={720}
         {...otherProps}
