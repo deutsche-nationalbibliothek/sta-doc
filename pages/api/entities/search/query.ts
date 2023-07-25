8bb3c20c2940d19d984d300068e39978caad407f
Thomas Bauer <tbauer2811@googlemail.com>
2023-07-25 09:21:49 +0200

    Redesign search functionality in solr, multi-word and phrase search support.

--- pages/api/entities/search/query.ts
+++ pages/api/entities/search/query.ts
@@ -12,23 +12,57 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     query: string;
     start: string;
   };
-  const query = (client.escapeSpecialChars as (s: string) => string)(
-    requestedQuery
-  );
+  const buildQueryStatement = (requestedQuery: string) => {
+    let statementScore1 = '';
+    let statementScore2 = '';
+    let statementScore3 = '';
+    let statementScore4 = '';
+    let statementScore5 = '';
+    let statementScore6 = '';
+    const words = [];
+    const phraseSearch = requestedQuery.match(/"(.*?)"/g);
+    if (phraseSearch) {
+      words.push(...phraseSearch);
+    } else {
+      const trim = requestedQuery
+        .replace(/[^\w\s]/gi, ' ')
+        .replace(/\s+/g, ' ')
+        .trim()
+        .split(' ');
+      words.push(...trim);
+    }
+    words.map((word, index) => {
+      const scoreLevel1 = `headline.title:${word}^30`;
+      const scoreLevel2 = `headline.title:*${word}*^20`;
+      const scoreLevel3 = `headline-text-search:${word}^20`;
+      const scoreLevel4 = `headline-text-search:*${word}*^10`;
+      const scoreLevel5 = `staNotationLabel:${word}*^10`;
+      const scoreLevel6 = `full-text-search:${word}^10`;
+      if (index == 0) {
+        statementScore1 += scoreLevel1;
+        statementScore2 += scoreLevel2;
+        statementScore3 += scoreLevel3;
+        statementScore4 += scoreLevel4;
+        statementScore5 += scoreLevel5;
+        statementScore6 += scoreLevel6;
+      } else {
+        statementScore1 += ' AND ' + scoreLevel1;
+        statementScore2 += ' AND ' + scoreLevel2;
+        statementScore3 += ' AND ' + scoreLevel3;
+        statementScore4 += ' AND ' + scoreLevel4;
+        statementScore5 += ' AND ' + scoreLevel5;
+        statementScore6 += ' AND ' + scoreLevel6;
+      }
+    });
+    return `((${statementScore1}) OR (${statementScore2}) OR (${statementScore3}) OR (${statementScore4}) OR (${statementScore5}) OR (${statementScore6}))`;
+  };
+  const query = buildQueryStatement(requestedQuery);
   const solrQuery = client
     .query()
-    .q(
-      `\
-headline.title:${query}^40 \
-OR headline.title:${query}*^30 \
-OR headline-text-search:${query}^20 \
-OR headline-text-search:${query}*^10 \
-OR full-text-search:${query}* \
-`
-    )
+    .q(`${query}`)
+    .qop('AND')
     .sort({ score: 'desc' })
     .fl(`*,score`)
-    // .qop('AND')
     .rows(10);
 
   if (start) {
