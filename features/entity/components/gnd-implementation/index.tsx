99848b8e13a55a9fbd03ccecbfc6c0a8d0f2f1f7
Thomas Bauer <tbauer2811@googlemail.com>
2023-10-18 10:55:38 +0200

    New feature for gnd-implementation textbox.

---
+++ features/entity/components/gnd-implementation/index.tsx
@@ -0,0 +1,7 @@
+import { useCodingsPreference } from '@/hooks/use-codings-preference';
+import { Entity } from '@/types/parsed/entity';
+import { Card, Col, Divider, Row, Select, Typography, theme } from 'antd';
+import {
+  GNDImplementation,
+  GNDImplementationProps,
+} from './gnd-implementation';
