38b6c6f9d7764d24fceefbe5f7ac965497d3b6cd
Thomas Bauer <tbauer2811@gmail.com>
2023-12-05 14:38:10 +0100

    Remove console logs.

--- bin/data/utils/fetch.ts
+++ bin/data/utils/fetch.ts
@@ -2,7 +2,6 @@ import fetch from 'node-fetch';
 import { API_URL } from '../fetcher';
 
 export const fetchWithSparql = (apiUrl: API_URL) => {
-  console.log('apiUrl', apiUrl);
   const sparqlQueryDispatcher = <T>(sparqlQuery: string) => {
     const headers = { Accept: 'application/sparql-results+json' };
     const path = `/query/proxy/wdqs/bigdata/namespace/wdq/sparql?query=${encodeURIComponent(
