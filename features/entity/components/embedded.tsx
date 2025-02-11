8ff09186b881fc81d2c679cf14adbecf12a3886c
tbauer2811 <tbauer2811@googlemail.com>
2025-02-11 09:02:14 +0100

    Add edit Link pen for convenience.

--- features/entity/components/embedded.tsx
+++ features/entity/components/embedded.tsx
@@ -2,9 +2,11 @@ import { Collapse } from '@/components/collapse';
 import { Entity } from '@/types/parsed/entity';
 import { EntityDetails } from './details';
 import { NamespaceThemeConfigProvider } from '@/components/namespace-theme-config-provider';
-import { PlusCircleOutlined } from '@ant-design/icons';
+import { EditOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { theme } from 'antd';
 import { Tooltip } from 'antd';
 import { memo } from 'react';
+import { ExternalLink } from '@/components/external-link';
 
 interface EmbeddedProps {
   entity: Entity;
@@ -12,6 +14,8 @@ interface EmbeddedProps {
 
 export const Embedded: React.FC<EmbeddedProps> = memo(
   ({ entity }) => {
+  const websideUrl = process.env.NEXT_PUBLIC_URL as string;
+  const { token } = theme.useToken();
     return (
       <NamespaceThemeConfigProvider namespace={entity.namespace}>
         <Collapse
@@ -19,11 +23,30 @@ export const Embedded: React.FC<EmbeddedProps> = memo(
           labelOpen={entity.contextOfUseLabel}
           labelClosed={entity.contextOfUseLabel}
           extra={
-            entity.contextOfUseLabel ? (
-              <Tooltip title="Anwendungskontext">
-                <PlusCircleOutlined />
-              </Tooltip>
-            ) : undefined
+            <>
+              {entity.contextOfUseLabel ? (
+                <Tooltip title="Anwendungskontext">
+                  <PlusCircleOutlined />
+                </Tooltip>
+              ) : undefined}
+              {websideUrl === 'https://edit.sta.dnb.de' ? (
+                <ExternalLink
+                  css={{
+                    color: `${token.colorText} !important`,
+                    float: 'right',
+                    paddingRight: '3px'
+                  }}
+                  linkProps={{
+                    href: `${websideUrl}/entity/${entity.id}`,
+                  }}
+                >
+                  <>
+                    {' '}
+                    <EditOutlined />
+                  </>
+                </ExternalLink>
+              ) : undefined}
+            </>
           }
         >
           <EntityDetails embedded entity={entity} />
