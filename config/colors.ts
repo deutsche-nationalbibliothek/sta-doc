b5d515566c830423a1228f074df11ae20114f7d0
juergenkett <juergenkett@gmx.de>
2025-04-22 20:00:39 +0200

    Farbschema für Namespace "STA" ergänzt

--- config/colors.ts
+++ config/colors.ts
@@ -1,7 +1,7 @@
 // duplicates styles/colors.css
 const colors = {
   'unspecific-namespace-color': '#979696',
-  'sta-color': '#858585',
+  'sta-color': '#898995',
   'rda-color': '#fd7b15',
   'gnd-color': '#0068FF',
   'light-gray': '#F0F0F0',
