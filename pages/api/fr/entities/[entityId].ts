810dc071a5344bfffd2533046b6980f51043f4ec
kett <juergenkett@gmx.de>
2025-06-24 17:40:31 +0200

    - Changed localization support. Fetch now appends a Query-Parameter 'locale=<locale>' to the API call. The api uses this parameter to get a localized version of the ressource from the entityRepository.

--- pages/api/fr/entities/[entityId].ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { getEntityJson } from '../../entities/[entityId]';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    return getEntityJson("fr", req, res);
-};
