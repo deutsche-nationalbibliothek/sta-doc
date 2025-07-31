3c12405ff820ec1fce54ce6628cefe16969dc7d5
Thomas Bauer <tbauer2811@googlemail.com>
2025-07-31 15:44:01 +0200

    Handle locale switcher select update.

--- components/locale-switcher.tsx
+++ components/locale-switcher.tsx
@@ -1,14 +1,23 @@
 import { useRouter } from 'next/router';
 import { Select } from 'antd';
+import { useState, useEffect } from 'react';
 
 const LocaleSwitcher = () => {
   const router = useRouter();
   const { locale: activeLocale } = router;
+  const [selectedLocale, setSelectedLocale] = useState(router.locale);
+
+  useEffect(() => {
+    if (router.locale !== selectedLocale) {
+      setSelectedLocale(router.locale);
+    }
+  }, [router.locale, selectedLocale]);
 
   const handleChange = async (value: string) => {
-    const { pathname, asPath, query } = router;
+    setSelectedLocale(value);
+    const { pathname, asPath } = router;
     try {
-      await router.push({ pathname, query }, asPath, { locale: value });
+      await router.push({ pathname, query: {} }, asPath, { locale: value });
     } catch (error) {
       console.error('Error changing locale:', error);
     }
@@ -19,7 +28,7 @@ const LocaleSwitcher = () => {
 
   return (
     <Select
-      defaultValue={activeLocale}
+      value={activeLocale}
       onChange={handleSelectChange}
       style={{ width: 70 }}
       options={[
