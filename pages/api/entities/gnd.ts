b9861996bf245ed7bba1ad8df80febe279c0e399
Thomas Bauer <tbauer2811@googlemail.com>
2025-07-31 15:46:42 +0200

    Index pages multilang support. WIP

--- pages/api/entities/gnd.ts
+++ pages/api/entities/gnd.ts
@@ -1,8 +1,7 @@
 import { entityRepository } from '@/features/entity/entity-repository';
 import { Namespace } from '@/types/namespace';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getLocaleFromReq } from '@/utils/locale-utils';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(entityRepository.getEntityIndexByNamespace(getLocaleFromReq(req), Namespace.GND));
+  res.status(200).json(entityRepository.getEntityIndexByNamespace(req.query.locale as string, Namespace.GND));
 };
