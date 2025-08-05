ef0911f84fd54158369a9e177f467bc700e6a8e0
Thomas Bauer <tbauer2811@googlemail.com>
2025-08-05 14:51:31 +0200

    Update docker image build script.

--- bin/build_and_transfer_docker_image.sh
+++ bin/build_and_transfer_docker_image.sh
@@ -15,16 +15,16 @@ IP_ADDRESS=$(grep IP_ADDRESS= .env | cut -d '=' -f 2)
 #npm run docker:build nextjs
 
 # Tag the Docker image
-echo "Tagging the Docker image..."
-docker tag sta-doc-nextjs sta-doc_nextjs
+# echo "Tagging the Docker image..."
+# docker tag sta-doc-nextjs sta-doc_nextjs
 
 # Save the Docker image to a tar file
 echo "Saving the Docker image to a tar file..."
-docker save sta-doc_nextjs:latest > sta-doc-multilingual-edit.tar
+# docker save sta-doc_nextjs:latest > sta-doc-nextjs.tar
+docker save sta-doc-nextjs:latest > sta-doc-nextjs.tar
 
 # Copy the tar file to a remote server using scp
 echo "Copying the tar file to a remote server..."
-scp sta-doc-multilingual-edit.tar wikiuser@$IP_ADDRESS:/home/wikiuser/sta-doc/
-#ssh wikiuser@10.10.14.210 "mkdir -p /home/wikiuser/docker_images && tar -xvf -" < sta-doc-multilingual-edit.tar
+scp sta-doc-nextjs.tar wikiuser@$IP_ADDRESS:/home/wikiuser/sta-doc/
 
 echo "Jobs done."
