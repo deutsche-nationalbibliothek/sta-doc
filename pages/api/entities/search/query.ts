8e2c996d4f7ed409058a30ae9774573255c975ab
Imke Högden <imke.hoegden@posteo.de>
2026-09-04 14:27:35 +0200

    fix: handle phrase search separately

--- pages/api/entities/search/query.ts
+++ pages/api/entities/search/query.ts
@@ -26,7 +26,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const buildQueryStatement = (requestedQuery: string) => {
     const phraseSearch = requestedQuery.match(/"(.*?)"/g); // macht daraus ein Array
 
-    console.log(phraseSearch)
+    console.log("phaseSearch:", phraseSearch)
     
     let statementScore1 = '';
     let statementScore2 = '';
@@ -38,12 +38,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (phraseSearch) {
       const phrases = phraseSearch;
 
-    
-    phrases.map((phrase, index) => {const scoreLevel1 = `headline.title:${phrase}^30`;
-    const scoreLevel2 = `headline.title:*${phrase}*^20`;
+    phrases.map((phrase, index) => {
+    const scoreLevel1 = `headline.title:${phrase}^30`;
+    const scoreLevel2 = `headline.title:*${phrase}*^20`; // Brauchen wir den Score hier? Funktioniert wildcard wie angenommen?
     const scoreLevel3 = `headline-text-search:${phrase}^20`;
-    const scoreLevel4 = `headline-text-search:*${phrase}*^10`;
-    const scoreLevel5 = `full-text-search:${phrase}^10`;})
+    const scoreLevel4 = `headline-text-search:*${phrase}*^10`; // Hier genauso. Brauchen wir den Score hier?
+    // ScoreLevel5 mit staNotation rausgelassen
+    const scoreLevel6 = `full-text-search:${phrase}^10`;
+    if (index == 0) {
+      statementScore1 += scoreLevel1;
+      statementScore2 += scoreLevel2;
+      statementScore3 += scoreLevel3;
+      statementScore4 += scoreLevel4;
+      // hier wieder staNotation rausgelassen
+      statementScore6 += scoreLevel6;
+    } else {
+      statementScore1 += ' AND ' + scoreLevel1;
+      statementScore2 += ' AND ' + scoreLevel2;
+      statementScore3 += ' AND ' + scoreLevel3;
+      statementScore4 += ' AND ' + scoreLevel4;
+      // hier wieder staNotation rausgelassen
+      statementScore6 += ' AND ' + scoreLevel6;
+    }});
+
+    return `((${statementScore1}) OR (${statementScore2}) OR (${statementScore3}) OR (${statementScore4}) OR (${statementScore6}))`;
     
     } else {
       const words = requestedQuery
@@ -52,8 +70,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         .trim()
         .split(' '); // ist am Ende auch ein Array
 
-      console.log(words)
-      // words.push(...trim);
+      console.log("words", words)
       words.map((word, index) => {
         const scoreLevel1 = `headline.title:${word}^30`;
         const scoreLevel2 = `headline.title:*${word}*^20`;
@@ -78,16 +95,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           statementScore6 += ' AND ' + scoreLevel6;
         }
       });
+
+      return `((${statementScore1}) OR (${statementScore2}) OR (${statementScore3}) OR (${statementScore4}) OR (${statementScore5}) OR (${statementScore6}))`;
     }
-    return `((${statementScore1}) OR (${statementScore2}) OR (${statementScore3}) OR (${statementScore4}) OR (${statementScore5}) OR (${statementScore6}))`;
   };
+  
   const query = buildQueryStatement(requestedQuery);
 
   // const query = `(headline.title:"bewegtes bild") OR (headline.title:*"bewegtes bild"*) OR (headline-text-search:"bewegtes bild") OR (headline-text-search: *"bewegtes bild"*) OR (staNotationLabel:"bewegtes bild") OR (staNotationLabel:"bewegtes bild"*) OR (full-text-search:"bewegtes bild")`;
 
   // const query = `((headline.title:"bewegtes bild"^30) OR (headline.title:*"bewegtes bild"*^20) OR (headline-text-search:"bewegtes bild"^20) OR (headline-text-search:*"bewegtes bild"*^10) OR ((staNotationLabel:"bewegtes bild"^50 OR staNotationLabel:"bewegtes bild"*^40)) OR (full-text-search:"bewegtes bild"^10))`;
-
-  // const query = `(staNotationLabel:"bewegtes bild" OR staNotationLabel:"bewegtes bild"*)`;
 
   // const query = `staNotationLabel:"bewegtes bild"* OR staNotationLabel:"bewegtes bild"`; // liefert 0 Treffer
 
