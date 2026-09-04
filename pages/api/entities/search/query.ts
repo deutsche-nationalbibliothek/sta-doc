be3e914c9a533237d6b5a4ca06d3e159ac504b0f
Imke Högden <imke.hoegden@posteo.de>
2026-09-04 13:33:32 +0200

    WIP: refactor search phrases

--- pages/api/entities/search/query.ts
+++ pages/api/entities/search/query.ts
@@ -24,47 +24,61 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(requestedQuery, start); // hinterher wieder entfernen
 
   const buildQueryStatement = (requestedQuery: string) => {
+    const phraseSearch = requestedQuery.match(/"(.*?)"/g); // macht daraus ein Array
+
+    console.log(phraseSearch)
+    
     let statementScore1 = '';
     let statementScore2 = '';
     let statementScore3 = '';
     let statementScore4 = '';
     let statementScore5 = '';
     let statementScore6 = '';
-    const words = [];
-    const phraseSearch = requestedQuery.match(/"(.*?)"/g);
+    
     if (phraseSearch) {
-      words.push(...phraseSearch);
+      const phrases = phraseSearch;
+
+    
+    phrases.map((phrase, index) => {const scoreLevel1 = `headline.title:${phrase}^30`;
+    const scoreLevel2 = `headline.title:*${phrase}*^20`;
+    const scoreLevel3 = `headline-text-search:${phrase}^20`;
+    const scoreLevel4 = `headline-text-search:*${phrase}*^10`;
+    const scoreLevel5 = `full-text-search:${phrase}^10`;})
+    
     } else {
-      const trim = requestedQuery
+      const words = requestedQuery
         .replace(/[^\w\s]/gi, ' ')
         .replace(/\s+/g, ' ')
         .trim()
-        .split(' ');
-      words.push(...trim);
+        .split(' '); // ist am Ende auch ein Array
+
+      console.log(words)
+      // words.push(...trim);
+      words.map((word, index) => {
+        const scoreLevel1 = `headline.title:${word}^30`;
+        const scoreLevel2 = `headline.title:*${word}*^20`;
+        const scoreLevel3 = `headline-text-search:${word}^20`;
+        const scoreLevel4 = `headline-text-search:*${word}*^10`;
+        const scoreLevel5 = `(staNotationLabel:${word}^50 OR staNotationLabel:${word.toLowerCase()}*^40)`;
+        const scoreLevel6 = `full-text-search:${word}^10`;
+        
+        if (index == 0) {
+          statementScore1 += scoreLevel1;
+          statementScore2 += scoreLevel2;
+          statementScore3 += scoreLevel3;
+          statementScore4 += scoreLevel4;
+          statementScore5 += scoreLevel5;
+          statementScore6 += scoreLevel6;
+        } else {
+          statementScore1 += ' AND ' + scoreLevel1;
+          statementScore2 += ' AND ' + scoreLevel2;
+          statementScore3 += ' AND ' + scoreLevel3;
+          statementScore4 += ' AND ' + scoreLevel4;
+          statementScore5 += ' AND ' + scoreLevel5;
+          statementScore6 += ' AND ' + scoreLevel6;
+        }
+      });
     }
-    words.map((word, index) => {
-      const scoreLevel1 = `headline.title:${word}^30`;
-      const scoreLevel2 = `headline.title:*${word}*^20`;
-      const scoreLevel3 = `headline-text-search:${word}^20`;
-      const scoreLevel4 = `headline-text-search:*${word}*^10`;
-      const scoreLevel5 = `(staNotationLabel:${word}^50 OR staNotationLabel:${word.toLowerCase()}*^40)`; // hier liegt das problem
-      const scoreLevel6 = `full-text-search:${word}^10`;
-      if (index == 0) {
-        statementScore1 += scoreLevel1;
-        statementScore2 += scoreLevel2;
-        statementScore3 += scoreLevel3;
-        statementScore4 += scoreLevel4;
-        statementScore5 += scoreLevel5;
-        statementScore6 += scoreLevel6;
-      } else {
-        statementScore1 += ' AND ' + scoreLevel1;
-        statementScore2 += ' AND ' + scoreLevel2;
-        statementScore3 += ' AND ' + scoreLevel3;
-        statementScore4 += ' AND ' + scoreLevel4;
-        statementScore5 += ' AND ' + scoreLevel5;
-        statementScore6 += ' AND ' + scoreLevel6;
-      }
-    });
     return `((${statementScore1}) OR (${statementScore2}) OR (${statementScore3}) OR (${statementScore4}) OR (${statementScore5}) OR (${statementScore6}))`;
   };
   const query = buildQueryStatement(requestedQuery);
