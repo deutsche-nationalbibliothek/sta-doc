2a4502f63fc6286f59faea47de763d0111cf3a2d
Imke Högden <imke.hoegden@posteo.de>
2026-09-15 11:45:12 +0200

    refactor: update autocomplete for solr select response

--- components/search/solr.tsx
+++ components/search/solr.tsx
@@ -49,18 +49,28 @@ export const SolrSearch: React.FC<SolrSearchProps> = ({
             setQuery(value.replaceAll(/(<([^>]+)>)/g, ''))
           }
           defaultValue={query}
+          // options={
+          //   query && suggestionsResult?.spellcheck.suggestions[1]
+          //     ? suggestionsResult.spellcheck.suggestions[1].suggestion
+          //         .sort((s1, s2) => s2.freq - s1.freq)
+          //         .map((x, index) => ({
+          //           value: x.word,
+          //           key: index,
+          //           label: (
+          //             <StringValueComponent stringValue={{ value: x.word }} />
+          //           ),
+          //         }))
+          //     : []
+          // }
           options={
-            query && suggestionsResult?.spellcheck.suggestions[1]
-              ? suggestionsResult.spellcheck.suggestions[1].suggestion
-                  .sort((s1, s2) => s2.freq - s1.freq)
-                  .map((x, index) => ({
-                    value: x.word,
-                    key: index,
-                    label: (
-                      <StringValueComponent stringValue={{ value: x.word }} />
-                    ),
-                  }))
-              : []
+            query && suggestionsResult?.response.docs ? 
+            suggestionsResult.response.docs.map((doc, index) => ({
+              value: doc['headline.title'],
+              key: doc.id,
+              label:
+              <StringValueComponent stringValue={{ value: doc['headline.title'] }} />
+            }))
+            : []
           }
         >
           <Input.Search
