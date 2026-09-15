4eceea7c8eb469125cb728045e0f045f800e9175
Imke Högden <imke.hoegden@posteo.de>
2026-09-15 11:23:57 +0200

    refactor: replace spellcheck with select for suggestions

--- pages/api/entities/search/suggest.ts
+++ pages/api/entities/search/suggest.ts
@@ -6,13 +6,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     query: string;
     start: string;
   };
+
   const query = escapeSpecialChars(requestedQuery);
 
   // http://localhost:8983/solr/entities/spell?df=text&spellcheck.q=form&spellcheck=true&spellcheck.collateParam.q.op=AND&wt=xml&spellcheck.build=true
-  const suggestionsResult = await solrGet('spell', {
-    q: query,
-    spellcheck: true,
-    build: true,
+
+  // const suggestionsResult = await solrGet('spell', {
+  //   q: query,
+  //   spellcheck: true,
+  //   build: true,
+  // });
+
+  const suggestionsResult = await solrGet('select', {
+    q: `headline.title:*${query}*`, // search within field headline.title
+    fl: 'id,headline.title', // return only id and headline.title from field list
+    rows: 6,
   });
 
   res.status(200).json(suggestionsResult);
