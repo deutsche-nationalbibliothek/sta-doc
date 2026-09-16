b88f9553f96bac77a424890bd910b86f2a5169b9
Imke Högden <imke.hoegden@posteo.de>
2026-09-16 11:31:44 +0200

    feat: implement mode control logic

--- components/segmented-control.tsx
+++ components/segmented-control.tsx
@@ -1,15 +1,27 @@
-import { useState } from 'react';
 import { Segmented } from 'antd';
-import { FetchingParam } from '@/hooks/fetch-query-params-provider';
+import {
+  FetchingParam,
+  useFetchingQueryParams,
+} from '@/hooks/fetch-query-params-provider';
 
 export default function SegmentedControl() {
-  const [value, setValue] = useState<string>('prod');
+  const { query, setFetchingParam } = useFetchingQueryParams();
 
   const options = [
-    { value: undefined, label: 'Default' },
+    { value: 'default', label: 'Default Mode' },
     { value: FetchingParam.live, label: 'Live' },
     { value: FetchingParam.prod, label: 'Prod' },
   ];
 
-  return <Segmented options={options} value={value} onChange={setValue} />;
+  return (
+    <Segmented
+      options={options}
+      value={query.live ?? 'default'}
+      onChange={(value) => {
+        setFetchingParam(
+          value === 'default' ? undefined : (value as FetchingParam)
+        );
+      }}
+    />
+  );
 }
