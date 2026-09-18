9ddbb9e5d80d26bcc57178ed3d263dae7eeb405f
Imke Högden <imke.hoegden@posteo.de>
2026-09-18 15:39:00 +0200

    fix (segmented mode control): include isActive and red dot

--- components/segmented-mode-control.tsx
+++ components/segmented-mode-control.tsx
@@ -3,20 +3,58 @@ import {
   FetchingParam,
   useFetchingQueryParams,
 } from '@/hooks/fetch-query-params-provider';
+import { Span } from 'next/dist/trace';
+
+function SegmentedControlLabel({
+  label,
+  isActive,
+}: {
+  label: string;
+  isActive: boolean;
+}) {
+  return (
+    <span>
+      {isActive && <span>RED DOT</span>}
+      {label}
+    </span>
+  );
+}
 
 export default function SegmentedControl() {
   const { query, setFetchingParam } = useFetchingQueryParams();
 
+  const currentValue = query.live ?? 'default';
+
   const options = [
-    { value: 'default', label: 'NO MODE' },
-    { value: FetchingParam.live, label: 'LIVE' },
-    { value: FetchingParam.prod, label: 'PROD' },
+    {
+      value: 'default',
+      label: <SegmentedControlLabel label="NO MODE" isActive={false} />,
+    },
+    {
+      value: FetchingParam.prod,
+      label: (
+        <SegmentedControlLabel
+          label="PROD"
+          isActive={currentValue === FetchingParam.prod}
+        />
+      ),
+    },
+    {
+      value: FetchingParam.live,
+      label: (
+        <SegmentedControlLabel
+          label="LIVE"
+          isActive={currentValue === FetchingParam.live}
+        />
+      ),
+    },
   ];
 
   return (
     <Segmented
       options={options}
-      value={query.live ?? 'default'}
+      // value={query.live ?? 'default'}
+      value={currentValue}
       onChange={(value) => {
         setFetchingParam(
           value === 'default' ? undefined : (value as FetchingParam)
