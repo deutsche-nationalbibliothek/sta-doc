7a5e9dc4adc4ad4871a6e5cbd97c46c1321749a1
Imke Högden <imke.hoegden@posteo.de>
2026-09-25 13:28:48 +0200

    WIP: add page wikibase test

---
+++ pages/wikibase-test.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { fetcher } from '@/bin/data/fetcher';
+
+export default function WikibaseTest() {
+  const [entityId, setEntityId] = useState('');
+  const [result, setResult] = useState(null);
+
+  const handleFetch = async () => {
+    const wikibaseData = fetcher(); // stürzt ab
+    console.log(wikibaseData);
+  };
+
+  return (
+    <main>
+      <h1>Wikibase Test</h1>
+
+      <input
+        type="text"
+        value={entityId}
+        onChange={(e) => setEntityId(e.target.value)}
+        placeholder="insert a Q- or P-value"
+      />
+
+      <button onClick={handleFetch}>Fetch Wikibase Data</button>
+
+      <h2>Response</h2>
+
+      <pre>{/*JSON Response*/}</pre>
+    </main>
+  );
+}
