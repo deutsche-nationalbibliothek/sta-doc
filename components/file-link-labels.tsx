526c8f41f71629a68b113ae8e979d5a157c5ff80
Imke Högden <imke.hoegden@posteo.de>
2026-09-25 12:02:10 +0200

    fix: observe dynamically added file links

--- components/file-link-labels.tsx
+++ components/file-link-labels.tsx
@@ -2,19 +2,36 @@ import { useEffect } from 'react';
 
 export default function FileLinkLabels() {
   useEffect(() => {
-    document
-      .querySelectorAll<HTMLAnchorElement>('a[href*=".pdf"], a[href*=".pptx"]')
-      .forEach((link) => {
-        const text = link.textContent ?? '';
+    const updateFileLabels = () => {
+      document
+        .querySelectorAll<HTMLAnchorElement>(
+          'a[href*=".pdf"], a[href*=".pptx"]'
+        )
+        .forEach((link) => {
+          const text = link.textContent ?? '';
 
-        if (/\bPDF\b/i.test(text)) {
-          link.classList.add('has-pdf-label');
-        }
+          if (/\bPDF\b/i.test(text)) {
+            link.classList.add('has-pdf-label');
+          }
 
-        if (/\bPPTX?\b/i.test(text)) {
-          link.classList.add('has-ppt-label');
-        }
-      });
+          if (/\bpowerpoint\b/i.test(text)) {
+            link.classList.add('has-ppt-label');
+          }
+        });
+    };
+
+    updateFileLabels(); // call directly to detect links when page is loaded
+
+    const observer = new MutationObserver(updateFileLabels); // MutionOberserver is able to observe the DOM and determine if something is being added, removed, or changed.
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    return () => {
+      observer.disconnect(); // cleanup function auf
+    };
   }, []);
 
   return null;
